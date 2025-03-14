31c31d44d0d9ac9688df8ae904cef1c436129518
FatihPehlevan <fatihpehlevan08@gmail.com>
2025-03-14 23:53:23 +0300

    first commit

---
+++ src/Content.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Content() {
+    //const 
+    const handleNameChange = () => {
+        const names = ["Bob", "Kevin", "Dave", "Hang"];
+        const int = Math.floor(Math.random() * 4);
+        return names[int];
+      }
+    const handleClick = () => {
+        console.log("You clicked it!")
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+  return (
+    <main>
+        <p onDoubleClick={handleClick}>
+            Hello {handleNameChange()}!
+        </p>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={() => handleClick2("Dave")}>Click It</button>
+        <button onClick={(e) => handleClick3(e)}>Click It</button>
+    </main>
+  )
+}
+
+export default Content
